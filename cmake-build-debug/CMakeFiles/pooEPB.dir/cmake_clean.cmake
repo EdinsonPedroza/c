fc3337c61ed33dbbd05bd73f3dc49e340480b9c7
retro021 <include.tkd@gmail.com>
2021-03-10 18:59:22 -0500

    primer commit #1

---
+++ cmake-build-debug/CMakeFiles/pooEPB.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pooEPB.dir/main.cpp.obj"
+  "libpooEPB.dll.a"
+  "pooEPB.exe"
+  "pooEPB.exe.manifest"
+  "pooEPB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pooEPB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
